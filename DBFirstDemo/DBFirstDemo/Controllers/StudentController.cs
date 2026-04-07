20b0a6c94bceee75f600aa35dd777322f54830a2
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-04-07 22:04:07 +0700

    Create StudentController and get data

---
+++ DBFirstDemo/DBFirstDemo/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using DBFirstDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBFirstDemo.Controllers
+{
+    public class StudentController : Controller
+   
+    {
+        private readonly CodeFirstDbContext _context;
+
+        public StudentController(CodeFirstDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _context.Students.ToList();
+            return View(list);
+        }
+    }
+}
+}
